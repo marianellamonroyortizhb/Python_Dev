4c939226b0a0814998aee7caf426274d8f62ce7d
marianellamonroyortizhb <1880@holbertonschool.com>
2021-06-14 21:40:28 -0500

    Create folder 03-operators

---
+++ Master en Python - Django, Flask, Tkinter/03-operators/01-arithmetic.py
@@ -0,0 +1,16 @@
+number1 = 12
+number2 = 4
+
+add = number1 +number2
+substr = number1 - number2
+mult = number1 * number2
+div = number1 / number2
+mod = number1 % number2
+
+print("Addition: " + str(add))
+print("Substraction: " + str(substr))
+print("Multiplication: " + str(mult))
+print("Division: " + str(div))
+print("Modulo: " + str(mod))
+
+
